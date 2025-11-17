593074992d84827723f064450e661eeb1531bb85
Yann-Karvelas <151407381+Yann-Karvelas@users.noreply.github.com>
2025-11-17 22:47:07 +0200

    Delete synthetic_codebase/src/data_fetcher.js

--- synthetic_codebase/src/data_fetcher.js
+++
@@ -1,9 +0,0 @@
-// Finding 5: Insecure HTTP
-fetch('http://api.example.com/data')
-  .then(res => res.json())
-  .then(data => console.log(data));
-
-// Finding 6: console.log in production
-function debug(msg) {
-  console.log('DEBUG:', msg);
-}
